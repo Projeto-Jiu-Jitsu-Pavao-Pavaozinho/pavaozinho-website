f3f2c2fcc296c79d81666bf7ff1a675203f24f6d
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2023-11-22 15:19:26 -0300

    implementação de galeria

---
+++ src/app/galery/galery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-galery',
+  templateUrl: './galery.component.html',
+  styleUrls: ['./galery.component.css']
+})
+export class GaleryComponent {
+
+}
