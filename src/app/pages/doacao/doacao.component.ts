e9a22733c689c7005aebff338a0cd6854a3ecb5d
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2023-12-18 22:31:30 -0300

    fix: ajuste no arquivo style

--- src/app/pages/doacao/doacao.component.ts
+++ src/app/pages/doacao/doacao.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-doacao',
   templateUrl: './doacao.component.html',
-  styleUrls: ['../../section-style.css']
+  styleUrls: ['../../section-style.css', '../../text-general.css']
 })
 export class DoacaoComponent {
 
