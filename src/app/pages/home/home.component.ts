c0f96e5d7b908f4639778a2bad484338977f76a0
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-04-17 22:39:39 -0300

    feat: método para inscrição na newsletter d atributos de controler

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NewsletterService } from 'src/app/services/newsletter.service';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,24 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   public newsletterSubSuccess: boolean = false;
+  public emailInput: string = "";
+  public ongoin: boolean = false;
+
+  public constructor(private newsletterService: NewsletterService) {}
 
   public toogleNewsletterAlert() {
     this.newsletterSubSuccess = !this.newsletterSubSuccess;
+  }
+
+  public newsletterSubscribe(email: String) {
+    if(email != "") {
+      this.newsletterSubSuccess = false;
+      this.ongoin = true;
+      this.newsletterService.subscribe(email).subscribe({
+        next: res => {this.newsletterSubSuccess = true; this.emailInput = ""},
+        error: err => {this.ongoin = false; console.log(err.status)},
+        complete: () => this.ongoin = false,
+      });
+    }
   }
 }
