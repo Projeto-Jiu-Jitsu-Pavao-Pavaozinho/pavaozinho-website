d0de876386e18e08e7de3d6207a5428c1911ec50
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-04-22 16:33:14 -0300

    refector: mudando o método subscribe para transmitir signais sobre o status da requisição HTTP e sinais indicando se há uma requisição em andamento

--- src/app/services/newsletter.service.ts
+++ src/app/services/newsletter.service.ts
@@ -1,23 +1,36 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpStatusCode } from '@angular/common/http';
+import { Injectable, WritableSignal, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsletterService {
+  public ongoin: WritableSignal<boolean> = signal(false);
+  public responsStatus: WritableSignal<HttpStatusCode> = signal(HttpStatusCode.Processing);
 
   private url: string = "https://newsletter-jm47.onrender.com";
 
   constructor(private http: HttpClient) { }
 
   public subscribe(email: String) {
+    this.ongoin.update(() => true);
+
     const body = {
       "email": email
     }
 
-    return this.http.post<any>(`${this.url}`, body).pipe(
-      res => res,
-      error => error
-    )
+    this.http.post<any>(`${this.url}`, body).subscribe({
+      next: res => {this.responsStatus.update(() => HttpStatusCode.Created)},
+      error: err => {
+        this.ongoin.update(() => false);
+        if(err.status == 409) {
+          this.responsStatus.update(() => HttpStatusCode.Conflict);
+          console.log(this.responsStatus());
+        } else {
+          this.responsStatus.update(() => HttpStatusCode.InternalServerError);
+        }
+      },
+      complete: () => this.ongoin.update(() => false)
+    })
   }
 }
