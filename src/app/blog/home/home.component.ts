b6c189af6d7cdd3d347012cf7221e74a9df093ce
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-05-31 16:04:22 -0300

    feat: adicionando meta tags ao inicializar o componente e removendo ao destruir

--- src/app/blog/home/home.component.ts
+++ src/app/blog/home/home.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DatoService } from 'src/app/services/dato.service';
 import { BlogPost } from '../models/blog-post';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css', '../../section-style.css', '../../text-general.css']
 })
-export class HomeComponent implements OnInit{
-  constructor(private datorService: DatoService) {
+export class HomeComponent implements OnInit, OnDestroy{
+  constructor(private datoService: DatoService, private meta: Meta) {
   }
 
   public posts: Array<BlogPost> = [];
 
   ngOnInit(): void {
-    this.datorService.getAllBlogPosts().subscribe({
-      next: res => { this.posts = res.data.allPosts},
+    this.datoService.getAllBlogPosts().subscribe({
+      next: res => {
+        this.posts = res.data.allPosts;
+        this.meta.addTag({name: 'description', content: 'Acompanhe as novidades que acontecem no Projeto Pavãozinho. Aqui postamos notícias sobre nossas atividade e artigos sobre esporte e outras reflexões.'});
+      },
       error: error => console.log(error)
     })
+  }
+
+  ngOnDestroy(): void {
+    this.meta.removeTag("name = 'description'");
   }
 }
